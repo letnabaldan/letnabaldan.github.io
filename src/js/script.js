7aa4901c972332a5a9df32157ccb5f34716c7359
Alexey <letnabaldan@gmail.com>
2020-11-06 13:08:55 +0900

    fifth

--- src/js/script.js
+++ src/js/script.js
@@ -33,4 +33,80 @@ $(document).ready(function () {
    toggleSlide('.catalog-item__link');
    toggleSlide('.catalog-item__back');
 
+   // modal window
+
+   $('[data-modal=consultation]').on('click', function () {
+      $('.overlay, #consultation').fadeIn('slow');
+   });
+   $('.modal__close').on('click', function () {
+      $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+   });
+
+   $('.button_mini').each(function (i) {
+      $(this).on('click', function () {
+         $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+         $('.overlay, #order').fadeIn('slow');
+      });
+   });
+
+
+
+   function validateForms(form) {
+      $(form).validate({
+         rules: {
+            name: "required",
+            phone: "required",
+            email: {
+               required: true,
+               email: true
+            }
+         },
+         messages: {
+            name: "Пожалуйста, введите свое имя",
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+               required: "Пожалуйста, введите свою почту",
+               email: "Неправильно введен адрес почты"
+            }
+         }
+      });
+   }
+   validateForms('#consultation-form');
+   validateForms('#consultation form');
+   validateForms('#order form');
+
+   $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+   $('form').submit(function (e) {
+      e.preventDefault();
+      $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            date: $(this).serialize()
+         })
+         .done(function () {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+         });
+      return false;
+   });
+   // smooth scroll and pageup
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > 1600) {
+         $('.pageup').fadeIn();
+      } else {
+         $('.pageup').fadeOut();
+      }
+   });
+   $("a[href^='#']").click(function () {
+      const _href = $(this).attr("href");
+      $("html, body").animate({
+         scrollTop: $(_href).offset().top + "px"
+      });
+      return false;
+   });
+
 });
